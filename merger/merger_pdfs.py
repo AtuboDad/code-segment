80b297d6bb058a0338bb8212c3e93943a1296ced
Chuanjian Lai <lcjasas@sina.com>
2024-04-19 09:19:07 +0800

    merger pdfs to single

--- merger/merger_pdfs.py
+++ merger/merger_pdfs.py
@@ -4,32 +4,19 @@ import PyPDF2
 output = PyPDF2.PdfWriter()
 
 # 打开第一个PDF文件
-with open('E:\\workspaces\\2024\\4月\\停车资料\\1.pdf', 'rb') as file1:
+with open('SCAN0000.pdf', 'rb') as file1:
     reader = PyPDF2.PdfReader(file1)
     # 将第一个PDF文件的所有页面添加到输出
     for page in reader.pages:
         output.add_page(page)
 
 # 打开第二个PDF文件
-with open('E:\\workspaces\\2024\\4月\\停车资料\\SCAN0001.pdf', 'rb') as file2:
+with open('SCAN0001.pdf', 'rb') as file2:
     reader = PyPDF2.PdfReader(file2)
     # 将第二个PDF文件的所有页面添加到输出
     for page in reader.pages:
         output.add_page(page)
-
-# 打开第二个PDF文件
-with open('E:\\workspaces\\2024\\4月\\停车资料\\SCAN0002.pdf', 'rb') as file2:
-    reader = PyPDF2.PdfReader(file2)
-    # 将第二个PDF文件的所有页面添加到输出
-    for page in reader.pages:
-        output.add_page(page)
-
-# 打开第二个PDF文件
-with open('E:\\workspaces\\2024\\4月\\停车资料\\SCAN0000.pdf', 'rb') as file2:
-    reader = PyPDF2.PdfReader(file2)
-    # 将第二个PDF文件的所有页面添加到输出
-    output.add_page(reader.pages[0])
 
 # 保存合并后的PDF文件
-with open('merged.pdf', 'wb') as merged_file:
+with open('Output.pdf', 'wb') as merged_file:
     output.write(merged_file)

c7d279ad87de40a67067825a34254a314c899fba
Chuanjian Lai <lcjasas@sina.com>
2024-07-25 23:14:53 +0800

    today do nothing

--- files/split-pdf/swap_pdf.py
+++ files/split-pdf/swap_pdf.py
@@ -3,7 +3,7 @@ from PyPDF2 import PdfWriter
 pdfWriter = PdfWriter()
  
 # 按顺序输入对应的页面编号
-pdfWriter.append('E:\\workspaces\\2024\\4月\\停车资料\\泉数空间合同.pdf', pages=[1,0])
+pdfWriter.append('example.pdf', pages=[1,0])
  
 pdfWriter.write('output.pdf')
 pdfWriter.close()
